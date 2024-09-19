75a41d40620347b7ec32260d50f1d5478bebad3e
Pxli9130 <peixuanli0319@gmail.com>
2024-09-19 01:19:45 -0400

    update for activity 5

---
+++ assets/js/projects.js
@@ -0,0 +1,55 @@
+const projects = [
+  {title: "Project 1",
+    description: "Description of project 1.",
+    imageUrl: "assets/img/ai-wallpaper-1.jpg",},
+  {title: "Project 2",
+    description: "Description of project 2.",
+    imageUrl: "assets/img/2373842_a332_2.jpg",},
+];
+let projectIndex =0;
+const projectsPerLoad =1;
+
+
+
+
+
+function displayProjects(index, count) {
+  const recentProjectsDiv = document.getElementById('recent-projects');
+  
+  for (let i = index; i < index + count && i < projects.length; i++) {
+      const project =projects[i];
+    const projectDiv =document.createElement('div');
+
+
+    
+    projectDiv.classList.add('project');
+    projectDiv.innerHTML = `
+    <div class="card medium">
+      <div class="card-image waves-effect waves-block waves-light">
+      <img src="${project.imageUrl}" alt="${project.title}" style="height: 100%; width: 100%">
+      </div>
+      <div class="card-content">
+      <span class="card-title teal-text">${project.title}</span>
+      <p>${project.description}</p>
+      </div>
+    </div>
+    `;
+    recentProjectsDiv.appendChild(projectDiv);
+  }
+  projectIndex+= count;
+  
+  if(projectIndex >= projects.length) 
+    {
+      document.getElementById('load-more-btn').style.display = 'none';
+    }
+  }
+  
+window.onload = () => {
+  displayProjects(projectIndex, projectsPerLoad);
+  };
+  
+
+
+function loadMoreProjects() {
+    displayProjects(projectIndex, projectsPerLoad);
+  }
